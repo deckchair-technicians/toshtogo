6e19464094ba053a118c86c779cd2a8cebb89560
Matt Savage <matthew.savage@gmail.com>
2014-02-05 19:11:04 +0000

    Fixes #21 If sender timeout is exceeded while processing request, it is not activated

--- src/toshtogo/util/core.clj
+++ src/toshtogo/util/core.clj
@@ -1,5 +1,6 @@
 (ns toshtogo.util.core
-  (:import (java.util UUID))
+  (:import (java.util UUID)
+           (java.util.concurrent TimeoutException))
   (:require
     [clojure.math.numeric-tower :refer [expt]]
     [clj-time.core :refer [after? now plus millis interval]]
@@ -69,30 +70,46 @@
 (defn sleep [millis]
   (Thread/sleep millis))
 
+(defmacro with-timeout [timeout-ms message & body]
+  (assert body)
+  `(if (nil? ~timeout-ms)
+     (do ~@body)
+     (let [f# (future-call (fn [] ~@body))]
+
+       (deref f# ~timeout-ms nil)
+
+       (if (realized? f#)
+         @f#
+         (do
+           (future-cancel f#)
+           (throw (TimeoutException. ~message)))))))
+
 (defn retry-until-success*
   ":interval        pause between retries, in millis
    :interval-fn     function that takes an integer for # of retries and return the # of millis to pause
    :timeout         number of millis after which we give up
    :error-fn        function to pass errors to"
   [func & {:keys [interval interval-fn timeout max-retries error-fn] :or {interval 10 error-fn nil} :as opts}]
-  (let [error-fn             (or error-fn (constantly nil))
+  (let [error-fn            (or error-fn (constantly nil))
         interval-fn         (if interval-fn interval-fn (fn [i] interval))
         started             (now)
         elapsed-time        (fn [] (interval started (now)))
         timeout-time        (when timeout (plus (now) (millis timeout)))
         timeout-expired?    (fn [] (and timeout-time (after? (now) timeout-time)))
         max-tries-exceeded? (fn [attempt-number] (and max-retries (>= attempt-number max-retries)))]
-    (loop [attempt-number 1
-           result (or-exception func)]
-      (if (first result)
-        (first result)
-        (if (or (timeout-expired?) (max-tries-exceeded? attempt-number))
-          (throw (RuntimeException.
-                   (str "Giving up on retry after" attempt-number "attempts and" (elapsed-time))
-                   (second result)))
-          (do (error-fn (second result))
-              (sleep (interval-fn attempt-number))
-              (recur (inc attempt-number) (or-exception func))))))))
+
+    (with-timeout timeout "Giving up on retry"
+      (loop [attempt-number 1
+             result (or-exception func)]
+        (if (first result)
+          (first result)
+          (if (or (timeout-expired?) (max-tries-exceeded? attempt-number))
+            (throw (RuntimeException.
+                     (str "Giving up on retry after" attempt-number "attempts and" (elapsed-time))
+                     (second result)))
+            (do (error-fn (second result))
+                (sleep (interval-fn attempt-number))
+                (recur (inc attempt-number) (or-exception func)))))))))
 
 (defmacro retry-until-success
   "opts "
