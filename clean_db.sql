9c30af3b6d29f8811651298c56a9f020cde6d884
Matt Savage <matthew.savage@gmail.com>
2014-02-12 16:40:11 +0000

    Added clean-db script for testing

---
+++ clean_db.sql
@@ -0,0 +1,12 @@
+﻿delete from commitment_outcomes;
+delete from agent_commitments;
+delete from contracts;
+
+delete from job_tags;
+delete from job_dependencies;
+delete from job_results;
+delete from jobs;
+
+delete from agents;
+
+delete from put_hashes;
