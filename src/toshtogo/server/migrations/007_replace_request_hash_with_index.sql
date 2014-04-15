a8b461fc6e22f3e29648a83226faea0a70f21a11
Matt Savage <matthew.savage@gmail.com>
2014-04-15 10:42:50 +0100

    Actually, better to fix original migration, which is broken for all databases with identical requests for different job types.

--- src/toshtogo/server/migrations/007_replace_request_hash_with_index.sql
+++ src/toshtogo/server/migrations/007_replace_request_hash_with_index.sql
@@ -1,5 +1,5 @@
 create index jobs_request_body_idx on jobs(request_body);
 
-alter table jobs add constraint jobs_unique_request_fungibility_group unique (request_body, fungibility_group_id);
+alter table jobs add constraint jobs_unique_request_fungibility_group unique (job_type, request_body, fungibility_group_id);
 
 alter table jobs drop column request_hash;
--- src/toshtogo/server/migrations/008_include_job_type_in_fungibility_index_check.sql
+++
@@ -1,2 +0,0 @@
-alter table jobs drop constraint jobs_unique_request_fungibility_group;
-alter table jobs add constraint jobs_unique_request_fungibility_group unique (job_type, fungibility_group_id, request_body);
