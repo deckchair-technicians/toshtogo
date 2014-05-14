b4c47982db84e982e87280f94049d530153eb6dc
Matt Savage <matthew.savage@gmail.com>
2014-05-14 13:29:54 +0100

    Explicit test for the issue with large job requests blowing Postgres' maximum size for indexed text fields

---
+++ test/toshtogo/test/functional/big_job_test.clj
@@ -0,0 +1,18 @@
+(ns toshtogo.test.functional.big-job-test
+  (:import (java.util UUID)
+           (toshtogo.client.senders SenderException))
+  (:require [midje.sweet :refer :all]
+            [clj-time.core :refer [now minutes seconds millis plus minus after? interval within?]]
+            [toshtogo.client.protocol :refer :all]
+            [toshtogo.util.core :refer [uuid uuid-str debug]]
+            [toshtogo.util.json :refer [decode]]
+            [toshtogo.test.functional.test-support :refer :all]))
+
+(background (before :contents @migrated-dev-db))
+
+(defn big-request []
+  {:data (take 1000 (repeatedly #(uuid-str)))})
+
+(future-fact
+  "We can handle large request bodies (current fails because the text is too big for Postgres to index)"
+  (put-job! client (uuid) (job-req (big-request) (uuid-str))))
