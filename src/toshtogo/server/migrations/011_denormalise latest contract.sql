f08c882a49fabb86b6cfe6a4ad0353db11368967
Matt Savage <matthew.savage@gmail.com>
2014-05-28 13:15:15 -0500

    Correcting migration SQL

--- src/toshtogo/server/migrations/011_denormalise latest contract.sql
+++ src/toshtogo/server/migrations/011_denormalise latest contract.sql
@@ -4,4 +4,4 @@ update jobs
   set latest_contract=contracts.contract_id
 from contracts
 where contracts.job_id = jobs.job_id
-  and contracts.contract_number=(select max(contract_number) from contracts c2 where c2.contract_id=contracts.contract_id);
+  and contracts.contract_number=(select max(contract_number) from contracts c2 where c2.job_id=jobs.job_id);
