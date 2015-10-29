007f4875ce41a1f1aff517c5652610f3f2ac0d25
Luke Baker <luke.baker@theorem.technology>
2015-10-29 15:38:31 +0000

    Add data.csv dependency

--- server/test/toshtogo/test/functional/benchmark_test.clj
+++ server/test/toshtogo/test/functional/benchmark_test.clj
@@ -1,49 +1,87 @@
 (ns toshtogo.test.functional.benchmark-test
   (:require [midje.sweet :refer :all]
             [clj-time.core :as t]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
             [toshtogo.util.core :refer [uuid uuid-str debug]]
             [toshtogo.test.functional.test-support :refer :all]
             [toshtogo.client.agent :refer :all]
             [toshtogo.client.protocol :refer :all]))
+
+(def persistent-client (test-client :timeout 100000))
+
+(defn write-csv [path row-data]
+  (let [columns (into [] (distinct (apply concat (map keys row-data))))
+        headers (map name columns)
+        rows (mapv #(mapv % columns) row-data)]
+    (with-open [file (io/writer path)]
+      (csv/write-csv file (cons headers rows)))))
+
+(defn mean
+  [values]
+  (/ (apply + values) (count values)))
+
+(defmacro timer
+  [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr]
+     (/ (double (- (. System (nanoTime)) start#)) 1000000.0)))
 
 (defn generate-dependent-job
   [job child-job-type id]
   (-> (job-req {:b (str "child " job)} child-job-type)
       (with-job-id id)))
 
+(defn get-job-by-id [n]
+  (let [number-of-dep-jobs n
+        job-id (uuid)
+        parent-job-type (uuid-str)
+        child-job-type (uuid-str)
+        child-job-seq (take number-of-dep-jobs (range))
+        child-job-type-seq (take number-of-dep-jobs (repeat child-job-type))
+        child-id-seq (take number-of-dep-jobs (repeatedly #(uuid)))
+        dep-seq (map generate-dependent-job
+                     child-job-seq
+                     child-job-type-seq
+                     child-id-seq)
+        actual-job-id (uuid)]
+
+    (put-job!
+      persistent-client
+      job-id (job-req
+               {:a "field value"} parent-job-type
+               :dependencies dep-seq))
+
+    (deref (future (while (-> (get-job client (last child-id-seq))
+                              :outcome
+                              (not= :success))
+                     (Thread/sleep 100)))
+           2000 nil)
+
+    (timer (get-job client actual-job-id))))
+
+(defn generate-profile
+  [f number-of-iterations number-of-dep-jobs]
+  (let [s #{}
+        profile (into s (for
+                          [iteration (map inc (range number-of-iterations))]
+                          (assoc
+                            {:iteration iteration
+                             :number-of-dep-jobs number-of-dep-jobs}
+                            :time-ms
+                            (f number-of-dep-jobs))))]
+    (sort-by :iteration profile)))
+
 (background
   (before :contents (reset-dev-db)))
 
-(with-redefs [toshtogo.client.protocol/heartbeat-time 10]
+(fact "Check the mean time of the get-job query with one job with a large number of dependencies"
+  (let [number-of-iterations-to-run 10
+        number-of-dependent-jobs 10
+        benchmark-in-ms 10
+        get-job-profile (generate-profile get-job-by-id
+                                          number-of-iterations-to-run
+                                          number-of-dependent-jobs)
+        mean-job-time (mean (map :time-ms get-job-profile))]
 
-  (fact "Time get-job query with one job with a large number of dependencies"
-    (let [number-of-dep-jobs 100
-          job-id (uuid)
-          parent-job-type (uuid-str)
-          child-job-type (uuid-str)
-          child-job-seq (take number-of-dep-jobs (range))
-          child-job-type-seq (take number-of-dep-jobs (repeat child-job-type))
-          child-id-seq (take number-of-dep-jobs (repeatedly #(uuid)))
-          dep-seq (map generate-dependent-job
-                       child-job-seq
-                       child-job-type-seq
-                       child-id-seq)
-          actual-job-id (uuid)]
-
-      (println :timimg-put)
-      (time
-        (put-job!
-          client
-          job-id (job-req
-                   {:a "field value"} parent-job-type
-                   :dependencies dep-seq)))
-
-      (deref (future (while (-> (get-job client (last child-id-seq))
-                                :outcome
-                                (not= :success))
-                       (Thread/sleep 100)))
-             2000 nil)
-
-      (println :actual (time (get-job client actual-job-id)))
-      ))
-  )
+    (< mean-job-time benchmark-in-ms) => truthy))
