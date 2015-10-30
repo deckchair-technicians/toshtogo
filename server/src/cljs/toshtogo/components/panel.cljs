0591dd8e46b620e7687e1aaa2548c3b368b8a242
James Carragher <james@carragher.org>
2015-10-30 13:52:42 +0000

    Add icon to make it obvious panels are collapsible

--- server/src/cljs/toshtogo/components/panel.cljs
+++ server/src/cljs/toshtogo/components/panel.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-always toshtogo.components.panel
   (:require [om.dom :as dom]
-            [om.core :as om]))
+            [om.core :as om]
+            [goog.string :as gstring]))
 
 (defn panel [{:keys [heading content]} owner]
   (reify
@@ -10,9 +11,12 @@
     om/IRenderState
     (render-state [_ {:keys [collapsed?]}]
       (dom/div #js {:className "panel panel-default"}
-               (dom/div #js {:className "panel-heading"
-                             :onClick   (fn []
-                                          (om/update-state! owner [:collapsed?] (partial not)))}
-                        heading)
-               (dom/div #js {:className (str "panel-body " (when collapsed? "collapse"))}
-                        content)))))
+        (dom/div #js {:className "panel-heading"
+                      :onClick   (fn []
+                                   (om/update-state! owner [:collapsed?] (partial not)))}
+          (dom/i #js {:className "fa fa-expand"
+                      :style     #js {:text-align "right"}})
+          (gstring/unescapeEntities "&nbsp;&nbsp;")
+          heading)
+        (dom/div #js {:className (str "panel-body " (when collapsed? "collapse"))}
+          content)))))
