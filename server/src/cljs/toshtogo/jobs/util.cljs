8a1826a74b4b11949fc50ced6ad7f037e8cb1a3d
Vesa Marttila <vesa.marttila@theorem.technology>
2015-11-02 16:47:43 +0000

    Verify job has an outcome when rendering status in UI

--- server/src/cljs/toshtogo/jobs/util.cljs
+++ server/src/cljs/toshtogo/jobs/util.cljs
@@ -1,9 +1,10 @@
 (ns toshtogo.jobs.util)
 
 (defn row-classname [job]
-  (case (keyword (:outcome job))
-    :waiting "info"
-    :error "danger"
-    :running "info"
-    :cancelled "warning"
-    (name (:outcome job))))
+  (when (:outcome job)
+    (case (keyword (:outcome job))
+      :waiting "info"
+      :error "danger"
+      :running "info"
+      :cancelled "warning"
+      (name (:outcome job)))))
